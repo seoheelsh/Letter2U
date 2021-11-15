4967a1aa776b3c65a0d3c925845cc659b82fb292
Seohee <seoheelsh@gmail.com>
2021-11-15 16:25:51 +0900

    new project

---
+++ src/main/java/com/letter2u/Letter2UApplication.java
@@ -0,0 +1,13 @@
+package com.letter2u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Letter2UApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Letter2UApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/letter2u/Letter2UApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letter2u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Letter2UApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
