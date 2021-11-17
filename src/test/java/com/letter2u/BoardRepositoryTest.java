1550dacc99842e5da23057b984d3eb6efd3d668c
Seohee <seoheelsh@gmail.com>
2021-11-17 21:30:11 +0900

    목록

--- src/test/java/com/letter2u/BoardRepositoryTest.java
+++ src/test/java/com/letter2u/BoardRepositoryTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.letter2u.board.domain.Board;
